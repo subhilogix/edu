9f4914f7222eff6cac86948a5a91f8fe8604002d
subhilogix <subhilogix@gmail.com>
2025-12-29 21:52:55 +0530

    PORTS ALTER

--- educycle-backend/app/main.py
+++ educycle-backend/app/main.py
@@ -12,8 +12,8 @@ app = FastAPI(
 # CORS (frontend will call this)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:5173", "http://127.0.0.1:8080", "http://127.0.0.1:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
